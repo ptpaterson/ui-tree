448848647dcd710691d884af038a45f3708a0a63
Paul <ptpaterson@gmail.com>
2017-08-07 22:55:09 -0400

    test main entry, improve Readme

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 # ui-tree [![npm](https://img.shields.io/npm/v/ui-tree.svg)](https://www.npmjs.com/package/ui-tree)
 
-
 > A UI Tree data structure for any front end, or none at all!
 
 ## Introduction
 
-I was looking for a way to manage varying sets of hierarchical data with the same library.  At the same time, I was unable to commit to a front-end, so I needed to keep the data structure library independent.
+I was looking for a way to work with several different sets of hierarchical data with the same library.  At the same time, I was unable to commit to a front-end, so I needed to keep the data structure library independent.
+
+### Features
+
+Still working on some of the basics...
+
+- [x] Simple API for simple data
+- [x] Lazy loading
+- [x] Custom data loading
+- [ ] Return nodes by field Value
+- [ ] Mask selection by ancestor Value
+- [ ] Filtering
+- [ ] Add/Remove/Insert nodes
 
 ## Installation
 
@@ -44,11 +55,11 @@ where each level has a `label` field, and a `children` field
 
 ## CreateTree options
 
-### lazy: Boolean
+### options.lazy: Boolean
 determines whether or not children nodes are automatically loaded
 
-### columns: Object[]
-Custom computed columns from any node information.  An array of Object, consisting of a `label` property, and a `Value(node)` function that performs calculation.
+### options.columns: Object[]
+Compute custom columns from any node information.  An array of `Object`, consisting of a `label` property, and a `Value(node)` function that performs calculation.
 
 Columns are computed and returned by a `treeNode.ReadColumns()` method.
 
@@ -60,21 +71,45 @@ var defaultColumns = [
 
 // ex. user columns
 var columns = [
-  { label: 'Level', Value (node) { return node.level } }
+  { label: 'Level', Value (node) { return node.level } },
   { label: 'Title', Value (node) { return node.data.title } }
 ]
 ```
 
-### Load: Function
-All children, even if not using lazy-loading, are loaded with through the same function, which can be overridden.  It is called from a tree-node, and passed in a resolve callback.  The resolve callback accepts an array of data objects for the children that will be used to build new tree-nodes.
+### options.Load(node, resolve): Function
+All children, even if not using "lazy-loading" (`options.lazy = true`), are loaded through the same function.  The `options.Load()` function can be passed in to override the default.  The function is called from a tree-node, and passed in a `resolve` callback.  The `resolve` callback accepts an array of data objects for the children that will be used to build new tree-nodes.
 
-The default `Load` function allows simple data to be processed
+parameters:
+- `node`: Reference to `treeNode` Object which is to be appended with children
+- `resolve`: Callback function that accepts an array of Objects to wrapped up into new `treeNode` children
+
+> NOTE: the `resolve` function must be called.  If there are no children to add, then and empty array should be passed in (`resolve([])`)
+
+The default `options.Load` function allows simple data to be processed
 
 ``` javascript
 // default Load
 Load: function (node, resolve) {
   if (node.data.children) {
     resolve(node.data.children)
+  } else {
+    resolve([])
+  }
+}
+```
+
+More complex data can be loaded.  The `options.Load` function is compatible with asynchronous or Promise functions.
+
+``` javascript
+// Load via some service
+Load: function (node, resolve) {
+  if (node.data.id) {
+    Database.SomeAjaxCallForChildren(node.data.id)
+      .then((children) => {
+        resolve(children)
+      })
+  } else {
+    resolve([])
   }
 }
 ```
--- test/test.js
+++ test/test.js
@@ -3,44 +3,20 @@
 const expect = require('chai').expect
 const assert = require('assert')
 
-const CreateTree = require('../lib/tree')
+const UiTree = require('../index.js')
 
-describe('Tree module', () => {
-  it('should export a function', () => {
-    expect(CreateTree).to.be.a('function')
-  })
+// *****************************************************************************
+// UiTree
+// *****************************************************************************
+describe('UiTree module', () => {
 
-  describe('w/ default options', () => {
-    var data = {
-      label: 'root',
-      children: [
-        { label: 'child1', children: [] },
-        { label: 'child2', children: [] }
-      ]
-    }
+  describe('.CreateTree(data, options)', () => {
 
-    var tree = CreateTree(data)
-
-    it('should create a simple tree with default options', () => {
-      expect(tree).to.not.equal(undefined)
+    it('should export a function', () => {
+      expect(UiTree.CreateTree).to.be.a('function')
     })
 
-    it('should create a node and attach to tree.root', () => {
-      expect(tree.root).to.not.equal(undefined)
-      expect(tree.root.data.label).to.equal('root')
-    })
-
-    it('children should be loaded automatically', () => {
-      expect(tree.root.children.length).to.equal(2)
-      expect(tree.root.children[0]).to.not.equal(undefined)
-      expect(tree.root.children[0].data.label).to.equal('child1')
-      expect(tree.root.children[1]).to.not.equal(undefined)
-      expect(tree.root.children[1].data.label).to.equal('child2')
-    })
-  })
-
-  describe('tree.lazy = true', () => {
-    it('children should be loaded manually', () => {
+    describe('w/ default options', () => {
       var data = {
         label: 'root',
         children: [
@@ -49,22 +25,53 @@ describe('Tree module', () => {
         ]
       }
 
-      var tree = CreateTree(data, { lazy: true })
-      expect(tree.root.children.length).to.equal(0)
+      var tree = UiTree.CreateTree(data)
 
-      tree.root.LoadChildren()
+      it('should create a simple tree with default options', () => {
+        expect(tree).to.not.equal(undefined)
+      })
 
-      expect(tree.root.children.length).to.equal(2)
-      expect(tree.root.children[0]).to.not.equal(undefined)
-      expect(tree.root.children[0].data.label).to.equal('child1')
-      expect(tree.root.children[1]).to.not.equal(undefined)
-      expect(tree.root.children[1].data.label).to.equal('child2')
+      it('should create a node and attach to tree.root', () => {
+        expect(tree.root).to.not.equal(undefined)
+        expect(tree.root.data.label).to.equal('root')
+      })
+
+      it('children should be loaded automatically', () => {
+        expect(tree.root.children.length).to.equal(2)
+        expect(tree.root.children[0]).to.not.equal(undefined)
+        expect(tree.root.children[0].data.label).to.equal('child1')
+        expect(tree.root.children[1]).to.not.equal(undefined)
+        expect(tree.root.children[1].data.label).to.equal('child2')
+      })
     })
-  })
 
-  describe('tree.Load = UserFunction', () => {
-    it.skip('children loaded using the user function', () => {
-      assert(false)
+    describe('w/ option lazy = true', () => {
+      it('children should be loaded manually', () => {
+        var data = {
+          label: 'root',
+          children: [
+            { label: 'child1', children: [] },
+            { label: 'child2', children: [] }
+          ]
+        }
+
+        var tree = UiTree.CreateTree(data, { lazy: true })
+        expect(tree.root.children.length).to.equal(0)
+
+        tree.root.LoadChildren()
+
+        expect(tree.root.children.length).to.equal(2)
+        expect(tree.root.children[0]).to.not.equal(undefined)
+        expect(tree.root.children[0].data.label).to.equal('child1')
+        expect(tree.root.children[1]).to.not.equal(undefined)
+        expect(tree.root.children[1].data.label).to.equal('child2')
+      })
+    })
+
+    describe('w/ option Load = UserFunction', () => {
+      it.skip('children loaded using the user function', () => {
+        assert(false)
+      })
     })
   })
 })
